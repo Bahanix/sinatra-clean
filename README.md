a71ccb10931da4dc2540be57d637dc678a8d3dd1
Julien Grillot <julien.grillot@gmail.com>
2015-07-21 14:21:58 +0200

    Users crud

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Quick start
 
 ```
-sudo apt-get install sqlite3
-git clone https://github.com/Bahanix/sinatra-ar.git
-cd sinatra-ar
+git clone https://github.com/Bahanix/sinatra-crud.git
+cd sinatra-crud/
 bundle install
-# ...
+bundle exec rake db:create
+bundle exec rake db:migrate
 bundle exec ruby app.rb -o 0.0.0.0
 ```
 
@@ -14,4 +14,4 @@ Then goto http://127.0.0.1:4567/
 ## Useful commands
 
 * `bundle exec rake -T` to show rake tasks
-* `irb -r ./app.rb` to debug your app
+* `bundle exec irb -r ./app.rb` to debug your app
--- app.rb
+++ app.rb
@@ -1,11 +1,35 @@
-require 'sinatra'
+require "sinatra"
 require "sinatra/activerecord"
-require 'slim'
+require "slim"
 
 class User < ActiveRecord::Base
 end
 
-get '/' do
+get "/" do
+  redirect "/users"
+end
+
+get "/users" do
   @users = User.all
   slim :index
+end
+
+post "/users" do
+  User.create name: params[:name]
+  redirect "/"
+end
+
+get "/users/:id" do
+  @user = User.find(params[:id])
+  slim :show
+end
+
+patch "/users/:id" do
+  User.find(params[:id]).update name: params[:name]
+  redirect "/users/#{params[:id]}"
+end
+
+delete "/users/:id" do
+  User.find(params[:id]).destroy
+  redirect "/"
 end
