f7efdfe027804de28dca40e8e84bfda96deb7337
Julien Grillot <julien.grillot@gmail.com>
2015-07-28 15:51:19 +0200

    Easier migration

--- db/migrate/20150719095544_create_users.rb
+++ db/migrate/20150719095544_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-    end
+    create_table :users
+    add_column :users, :name, :string
   end
 end
